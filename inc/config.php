96e18224de1c45b0d10e87750cef2a82fd31eedc
Rodrigoandfamily <30753668+Rodrigoandfamily@users.noreply.github.com>
2017-10-04 12:58:54 -0300

    Add files via upload

---
+++ inc/config.php
@@ -0,0 +1,8 @@
+<?php
+     //If there is no  defined called __CONFIG__, do not load this file
+      if(!defined('__CONFIG__'))   {
+        exit('You do not have a config file');
+      }
+     //our config is low
+
+ ?>
